f48357e7278da52efac005445bf79d813401aabf
Siavash Ahmadi <sia2156@gmail.com>
2026-04-18 16:07:55 -0400

    feat: add ThemeProvider and useTheme hook

---
+++ frontend/vitest.setup.ts
@@ -0,0 +1,16 @@
+// Polyfill matchMedia for jsdom environment
+const mockMatchMedia = (query: string) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: () => {},
+  removeListener: () => {},
+  addEventListener: () => {},
+  removeEventListener: () => {},
+  dispatchEvent: () => true,
+});
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: mockMatchMedia,
+});
